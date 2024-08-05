621ac2025ea59cff1233759baed34f4d86162fbe
femi-venus <femivenus@gmail.com>
2024-08-05 12:13:38 +0530

    written in reducehook

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,54 +1,85 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useReducer } from "react";
+import produce from "immer";
 import _ from "lodash";
-
 import "./todolist.css";
-type Tasks = { task: { input: string }; completed: boolean };
+import { Action, State } from "./types";
 
-export default function Todolist() {
-  return (
-    <div className="tdl">
-      <h1 className="tdl--title">todos</h1>
-      <List />
-    </div>
-  );
-}
+const initialState: State = {
+  input: "",
+  tasks: [],
+  filteredTasks: [],
+};
+
+const reducer = produce((state: State, action: Action) => {
+  switch (action.type) {
+    case "set_input":
+      state.input = action.payload;
+      break;
+    case "add_task":
+      state.tasks.push(action.payload);
+      state.input = ""; // Clear input after adding task
+      break;
+    case "change_status":
+      state.tasks[action.payload].completed =
+        !state.tasks[action.payload].completed;
+      break;
+    case "delete_task":
+      state.tasks.filter((task, index) => index !== action.payload);
+      break;
+    case "set_filtered_tasks":
+      state.filteredTasks = action.payload;
+      break;
+    case "clear_completed":
+      state.tasks = state.tasks.filter((task) => !task.completed);
+      break;
+    default:
+      break;
+  }
+});
 
 function List() {
-  const [input, setInput] = useState("");
-  const [tasks, setTasks] = useState<Tasks[]>([]);
-  const [filteredTasks, setFilteredTasks] = useState<Tasks[]>([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    setFilteredTasks(tasks);
-  }, [tasks]);
+    dispatch({ type: "set_filtered_tasks", payload: state.tasks });
+  }, [state.tasks]);
 
-  const handleChange = (e: { target: { value: string } }) => {
-    const newInput = e.target.value;
-    setInput(newInput);
+  const filterTasks = (filter: string) => {
+    let filteredTasks;
+    switch (filter) {
+      case "all":
+        filteredTasks = state.tasks;
+        break;
+      case "active":
+        filteredTasks = state.tasks.filter((task) => !task.completed);
+        break;
+      case "completed":
+        filteredTasks = state.tasks.filter((task) => task.completed);
+        break;
+      default:
+        filteredTasks = state.tasks;
+    }
+    dispatch({ type: "set_filtered_tasks", payload: filteredTasks });
   };
 
-  const handleKeyDown = (e: { key: string }) => {
-    if (e.key === "Enter") {
-      const newObject = { task: { input }, completed: false };
-      setTasks([...tasks, newObject]);
-      setInput("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: "set_input", payload: e.target.value });
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && state.input !== "") {
+      const newTask = { task: state.input, completed: false };
+      dispatch({ type: "add_task", payload: newTask });
     }
   };
+
   const handleCheckbox = (index: number) => {
-    setTasks((tasks) =>
-      tasks.map((task, i) =>
-        i === index ? { ...task, completed: !task.completed } : task
-      )
-    );
+    dispatch({ type: "change_status", payload: index });
   };
 
   const handleRemove = (index: number) => {
-    setTasks((tasks) => tasks.filter((task, i) => i !== index));
+    dispatch({ type: "delete_task", payload: index });
   };
-
-  const CompletedTasks = tasks.filter((task) => task.completed);
-  const ActiveTasks = tasks.filter((task) => !task.completed);
 
   return (
     <>
@@ -56,59 +87,54 @@ function List() {
         type="text"
         className="tdl--header"
         placeholder="What needs to be done?"
-        value={input}
+        value={state.input}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
       />
 
-      {_.size(tasks) !== 0 && (
+      {state.tasks.length > 0 && (
         <div className="tdl--body">
-          {filteredTasks.map((obj, index) => (
+          {state.filteredTasks.map((task, index) => (
             <div key={index} className="tdl--task">
               <input
                 type="checkbox"
                 className="tdl--checkbox"
-                checked={obj.completed}
+                checked={task.completed}
                 onChange={() => handleCheckbox(index)}
               />
-              {obj.task.input}
-
-              <button className="tdl--x" onClick={() => handleRemove(index)}>
+              {task.task}
+              <span className="tdl--x" onClick={() => handleRemove(index)}>
                 x
-              </button>
+              </span>
               <br />
             </div>
           ))}
           <div className="tdl--filters">
-            <button
-              className="tdl--btn-all"
-              onClick={() => {
-                setFilteredTasks(tasks);
-              }}
-            >
+            <span className="tdl--text">
+              {state.tasks.filter((task) => !task.completed).length} tasks left!
+            </span>
+            <button className="tdl--btn-all" onClick={() => filterTasks("all")}>
               All
             </button>
             <button
               className="tdl--btn-active"
-              onClick={() => {
-                setFilteredTasks(ActiveTasks);
-              }}
+              onClick={() => filterTasks("active")}
             >
               Active
             </button>
             <button
               className="tdl--btn-completed"
-              onClick={() => {
-                setFilteredTasks(CompletedTasks);
-              }}
+              onClick={() => filterTasks("completed")}
             >
               Completed
             </button>
             <button
               className="tdl--btn-clear"
-              onClick={() => {
-                setTasks(ActiveTasks);
-              }}
+              onClick={() =>
+                dispatch({
+                  type: "clear_completed",
+                })
+              }
             >
               Clear Completed
             </button>
@@ -116,5 +142,14 @@ function List() {
         </div>
       )}
     </>
+  );
+}
+
+export default function Todolist() {
+  return (
+    <div className="tdl">
+      <h1 className="tdl--title">todos</h1>
+      <List />
+    </div>
   );
 }
---
+++ src/components/types.ts
@@ -0,0 +1,28 @@
+export interface Task {
+  task: string;
+  completed: boolean;
+}
+
+export interface State {
+  input: string;
+  tasks: Task[];
+  filteredTasks: Task[];
+}
+
+export type Action =
+  | SET_INPUT
+  | ADD_TASK
+  | CHANGE_STATUS
+  | DELETE_TASK
+  | SET_FILTETERED_TASKS
+  | CLEAR_COMPLETED;
+
+export type SET_INPUT = { type: "set_input"; payload: string };
+export type ADD_TASK = { type: "add_task"; payload: Task };
+export type CHANGE_STATUS = { type: "change_status"; payload: number };
+export type DELETE_TASK = { type: "delete_task"; payload: number };
+export type SET_FILTETERED_TASKS = {
+  type: "set_filtered_tasks";
+  payload: Task[];
+};
+export type CLEAR_COMPLETED = { type: "clear_completed" };
